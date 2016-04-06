54a217b2479b22ad6a9f51e34ed7917cb714b184
nooraakhtar <noora.akhtar@euromonitor.com>
2016-04-06 15:21:55 +0530

    Moved listener class from Main Activity
    Moved listener class from Main Activity

--- app/src/main/java/com/example/nooraakhtar/listview/app/MainActivity.java
+++ app/src/main/java/com/example/nooraakhtar/listview/app/MainActivity.java
@@ -23,6 +23,8 @@ import android.widget.EditText;
 
 import com.example.nooraakhtar.listview.R;
 import com.example.nooraakhtar.listview.adapter.StoreCheckDetailAdapter;
+import com.example.nooraakhtar.listview.listener.ClickListener;
+import com.example.nooraakhtar.listview.listener.RecyclerTouchListener;
 import com.example.nooraakhtar.listview.model.StoreCheckDetail;
 
 
@@ -168,53 +170,6 @@ public class MainActivity extends ActionBarActivity {
             fragmentManager.beginTransaction()
                     .replace(R.id.containerFrame, fragment).commit();
             navigationFragment.closeDrawer();
-        }
-    }
-
-    public interface ClickListener {
-        void onClick(View view, int position);
-        void onLongClick(View view, int position);
-    }
-
-    public class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
-        private GestureDetector gestureDetector;
-        private MainActivity.ClickListener clickListener;
-
-        public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final MainActivity.ClickListener clickListener) {
-            this.clickListener = clickListener;
-            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
-                @Override
-                public boolean onSingleTapUp(MotionEvent e) {
-                    return true;
-                }
-
-                @Override
-                public void onLongPress(MotionEvent e) {
-                    View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
-                    if (child != null && clickListener != null) {
-                        clickListener.onLongClick(child, recyclerView.getChildPosition(child));
-                    }
-                }
-            });
-        }
-
-        @Override
-        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
-
-            View child = rv.findChildViewUnder(e.getX(), e.getY());
-            if (child != null && clickListener != null && gestureDetector.onTouchEvent(e)) {
-                clickListener.onClick(child, rv.getChildPosition(child));
-            }
-            return false;
-        }
-
-        @Override
-        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
-        }
-
-        @Override
-        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
-
         }
     }
 }
---
+++ app/src/main/java/com/example/nooraakhtar/listview/listener/ClickListener.java
@@ -0,0 +1,11 @@
+package com.example.nooraakhtar.listview.listener;
+
+import android.view.View;
+
+/**
+ * Created by Noora.Akhtar on 06/04/2016.
+ */
+public interface ClickListener {
+    void onClick(View view, int position);
+    void onLongClick(View view, int position);
+}
